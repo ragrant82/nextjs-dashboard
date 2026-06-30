4bb5797526eeda4da46b7349116fc3e48d481338
이신희 <ragrant@ipartners.co.kr>
2026-06-30 14:32:07 +0900

    nextjs dashboard ~15 (end)

---
+++ .history/nextjs-dashboard/app/layout_20260630142601.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+ 
+export default function RootLayout() {
+  // ...
+}
---
+++ .history/nextjs-dashboard/app/layout_20260630142705.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
